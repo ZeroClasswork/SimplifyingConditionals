8256115e58b4b43cc164515276cc856f062b1d1f
atleastzero <megan.obryan52@gmail.com>
2021-02-11 11:40:25 -0800

    Finished consolidate_conditional.py

---
+++ consolidate_conditional.py
@@ -0,0 +1,37 @@
+# by Kami Bigdely
+# Consolidate conditional expressions
+def dice(ingredients):
+    print("diced all ingredients.")
+def mix_all(diced_ingredients):
+    print("mixed all.")
+def add_salt():
+    print('added salt.')
+def pour(liquid):
+    print('poured', liquid + '.',)
+
+def lacks_ingredients(ingredients):
+    necessary_ingredients = {
+        'cucumber': False,
+        'tomato': False,
+        'onion': False,
+        'lemon juice': False
+    }
+    for ingredient in ingredients:
+        necessary_ingredients[ingredient] = True
+    for _, has_ingredient in necessary_ingredients.items():
+        if not has_ingredient:
+            return True
+    return False
+
+def make_shirazi_salad(ingredients):
+    if lacks_ingredients(ingredients):
+        print('lacks ingredients.')
+        return
+    dice(ingredients)
+    mix_all(ingredients)
+    add_salt()
+    pour('lemon juice')
+    print('Your yummy shirazi salad is ready!')
+
+if __name__ == "__main__":
+    make_shirazi_salad(['cucumber', 'tomato', 'lemon juice', 'onion'])
